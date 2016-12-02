77e13e4a8545ec3eb78715308c87b208cf11a2df
Mark <mguby814@gmail.com>
2016-12-02 16:55:45 -0600

    Refactor get requests into its own class to prevent code duplication

--- routes/constants.js
+++ routes/constants.js
@@ -1,6 +1,4 @@
-var secrets = require('../config/secrets');
-var mongoose = require('mongoose');
-var helpers = require('../config/helpers');
+var getter = require('../config/get_requester');
 var Races = require('../models/race');
 
 var baseRoute = '/constants';
@@ -9,22 +7,7 @@ module.exports = function(router) {
 
     var raceRoute = router.route(baseRoute + '/races');
     raceRoute.get(function(req, res) {
-        var json = helpers.parseQuery(req.query);
-        var db = mongoose.connect(secrets.mongo_connection);
-        Races.find(json['where'], json['select'])
-            .skip(json['skip'])
-            .limit(json['limit'])
-            .sort(json['sort'])
-            .exec(function(err, result) {
-                if(err === null) {
-                    err = 'OK';
-                }
-                if(json['count']) {
-                    result = result.length;
-                }
-                res.json(helpers.createResponse(err, result));
-            });
-        db.disconnect();
+        getter.get(Races, req, res);
     });
 
     return router;
