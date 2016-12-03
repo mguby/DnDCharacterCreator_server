99285ae489b6486e29914c44a2e162f24bf1094b
Mark <mguby814@gmail.com>
2016-12-02 20:38:57 -0600

    Update race schema

--- models/race.js
+++ models/race.js
@@ -1,9 +1,21 @@
 // Load required packages
 var mongoose = require('mongoose');
 
-// Define our beer schema
+var statSchema = new mongoose.Schema({
+    name: String,
+    modifier: Number
+});
+
+var traitSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    boost: [statSchema]
+});
+
 var RaceSchema   = new mongoose.Schema({
-    name: String
+    name: String,
+    description: String,
+    traits: [traitSchema]
 });
 
 // Export the Mongoose model
